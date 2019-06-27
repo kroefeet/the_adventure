737eb204b8bbefb426996e2983b64763da23ca6f
Joe Cera <subscribeto@runbox.com>
2019-06-27 14:22:49 -0700

    setting up types and locations lists

--- start.py
+++ start.py
@@ -1,6 +1,10 @@
+from types_and_locations import types, locations
+
 def main():
 	#
 	print("This is the start script for the adventure")
+	print(types)
+	print(locations)
 	
 	
 if __name__ == "__main__" :
---
+++ types_and_locations.py
@@ -0,0 +1,58 @@
+types = [
+	{
+		"name":"law journal article",
+		"description":"journal article from a law journal",
+	
+	},
+	{
+		"name":"non-law journal article",
+		"description":"journal article from a non-law journal",
+	},
+	{
+		"name":"book",
+		"description":"a physical copy of a book",
+	},
+	{
+		"name":"newspaper or magazine article",
+		"description":"an article from a newspaper or magazine",
+	},
+]
+
+locations = [
+	{
+		"name":"check shelf",
+		"description":"you find the call number and go to the location where the book should be",
+	},
+		{
+		"name":"google scholar",
+		"description":"you go to www.scholar.google.com and type in some information about the item",
+	},
+	{
+		"name":"publisher's website",
+		"description":"you go directly to the publisher's website to find the item",
+	},
+		{
+		"name":"author file",
+		"description":"you decide to contact the author(s) to see if they will provide the item",
+	},
+		{
+		"name":"database",
+		"description":"you check the database list to determine if there is an applicable database for this item",
+	},
+		{
+		"name":"e-journals list",
+		"description":"you check the e-journals list to check the availability by format and year",
+	},
+		{
+		"name":"microfilm",
+		"description":"you brave the darkest corners of the library to see if you can find the item in the microfilm collection",
+	},
+		{
+		"name":"ebook",
+		"description":"ebooks are often PDFs of the original so you search the catalog for an ebook",
+	},
+		{
+		"name":"inter-library loan",
+		"description":"the item is not available locally so you decide to try interlibrary loan",
+	},
+]
